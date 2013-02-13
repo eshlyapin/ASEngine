6a098a6453aeb414c168aaaa4a728d3f2f8ffc9c
eugene <eugene.s.shlyapin@gmail.com>
2013-02-14 03:31:00 +0400

    msvs project

---
+++ Client/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+
+namespace Client
+{
+    class User
+    {
+        public int x, y;
+        public string name;
+        public override string ToString()
+        {
+            return name + " position{ " + x + ", " + y + " }";
+        }
+    }
+    class Program
+    {
+        static List<User> users = new List<User>();
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 1234);
+            Stream stream = client.GetStream();
+
+            string userName = "Bot #" + rnd.Next();
+
+            StreamWriter wStream = new StreamWriter(stream);
+            wStream.Write(userName.Length);
+            wStream.Write(userName);
+
+            int x = rnd.Next();
+            int y = rnd.Next();
+
+
+            while (true)
+            {
+                wStream.Write(x);
+                wStream.Write(y);
+
+                User nu = new User();
+                byte[] buffer = new byte[8];
+                stream.Read(buffer, 0, 8);
+                nu.x = BitConverter.ToInt32(buffer, 0);
+                nu.y = BitConverter.ToInt32(buffer, 3);
+
+                stream.Read(buffer, 0, 4);
+                int len = BitConverter.ToInt32(buffer,0);
+                byte[] str = new byte[BitConverter.ToInt32(buffer, 0)];
+                stream.Read(str, 0, len);
+                nu.name = BitConverter.ToString(str, 0);
+
+                bool needToAdd = true;
+                Console.Clear();
+                for (int i = 0; i < users.Count; ++i)
+                {
+                    if (nu.name == users[i].name)
+                        needToAdd = false;
+                    Console.WriteLine(users[i].ToString());
+                }
+                if (needToAdd)
+                    users.Add(nu);
+            }            
+        }
+    }
+}
---
+++ Profol/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace Profol
+{
+    class UserData
+    {
+        public float x, y;
+    }
+
+    class User
+    {
+        public UserData Data { get; set; }
+        public bool Loggined = false;
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        TcpListener listener;
+        List<TcpClient> clientList = new List<TcpClient>();
+        Dictionary<TcpClient, User> users = new Dictionary<TcpClient, User>();
+
+        public Program()
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8089);
+            try
+            {
+                listener.Start(10);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Thread acceptThread = new Thread(
+                () =>
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    lock (clientList)
+                    {
+                        clientList.Add(client);
+                    }
+                });
+            acceptThread.Start();
+        }
+
+        bool Update()
+        {
+            if (clientList.Count > 0)
+            {
+                foreach (var client in clientList)
+                {
+                    User user = users[client];
+                    if (!user.Loggined)
+                    {
+                        try
+                        {
+                            ProcessLogin(user, client.GetStream());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        user.Data = ParseData(client.GetStream());
+                        foreach (var otherUser in users)
+                        {
+                            var otherPlayer = otherUser.Value;
+                            if(otherPlayer != user)
+                                SendUsersData(otherPlayer, client.GetStream());
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        void SendUsersData(User user, Stream stream)
+        {
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(user.Data.x);
+            writer.Write(user.Data.y);
+            writer.Write(user.Name.Length);
+            writer.Write(user.Name);
+        }
+
+        UserData ParseData(Stream stream)
+        {
+            UserData data = new UserData();
+
+            byte[] buffer = new byte[8];
+            stream.Read(buffer, 0, 8);
+
+            data.x = BitConverter.ToInt32(buffer, 0);
+            data.y = BitConverter.ToInt32(buffer, 3);
+
+            return data;
+        }
+
+        void ProcessLogin(User user, Stream stream)
+        {
+            int len = 0;
+            byte[] lenbuf = new byte[4];
+            stream.Read(lenbuf,0,4);
+            len = BitConverter.ToInt32(lenbuf, 0);
+            if (len > -1)
+            {
+                byte[] buffer = new byte[len];
+                int count = stream.Read(buffer, 0, len);
+                if (count != len)
+                    throw new Exception("Data wasn't recived blyad'!");
+
+                user.Name = BitConverter.ToString(buffer);
+                user.Loggined = true;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            while((new Program()).Update());
+        }
+    }
+}
